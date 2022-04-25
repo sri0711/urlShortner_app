20fc8725fb1db2d30092c7fe56f422f5fac229f2
sri0711 <m.sathish0711@gmail.com>
2022-04-25 22:57:11 +0530

    fix temp

--- pages/Chat.js
+++ pages/Chat.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,SafeAreaView,StyleSheet } from 'react-native';
+import React from 'react';
 
 const Chat = () => {
-  return (
-    <View>
-      <Text>Chat</Text>
-    </View>
-  )
-}
+	return (
+		<View style={chatStyle.main}>
+			<View style={chatStyle.header}>
+				<SafeAreaView>
+					<Text style={chatStyle.heading}>Url Shortner Chat</Text>
+				</SafeAreaView>
+			</View>
+		</View>
+	);
+};
 
-export default Chat
+const chatStyle = StyleSheet.create({
+	main: {
+		height: '100%',
+		backgroundColor: '#EEF2FF'
+	},
+    header: {
+		backgroundColor: '#f80000',
+		height: '15%',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	heading: {
+		alignSelf: 'center',
+		fontSize: 40,
+		color: '#EEF2FF',
+		fontWeight: '700'
+	},
+});
+
+export default Chat;
